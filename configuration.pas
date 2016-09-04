19153e43920111d7fd28aa8960bae673c1a2bef2
magorium <magorium@users.noreply.github.com>
2016-09-04 22:23:13 +0200

    Check-in unit configuration

---
+++ configuration.pas
@@ -0,0 +1,22 @@
+unit configuration;
+
+{$IFDEF AROS}
+{$MODE OBJFPC}{$H+}
+{$ENDIF}
+
+interface
+
+const
+  DEFAULT_HOST : PChar = 'localhost';
+  DEFAULT_PORT = 6600;
+
+implementation
+
+
+initialization
+
+
+finalization
+
+
+end.
